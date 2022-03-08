b2d3b3ad082d7f296422073f7528d3ad4f34f835
Simon Zorde <simon@zorde.dk>
2022-03-08 19:22:05 +0100

    init

---
+++ 2traceApp/Controllers/KundeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _2traceApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2traceApp.Controllers
+{
+    public class KundeController : Controller
+    {
+        // GET: KundeController
+        public ActionResult Kunde()
+        {
+            Kunde kunde = new Kunde();
+            kunde.Navn = "Simon Zorde";
+            kunde.Adresse = "Jægersborggade 16, 3. th";
+            kunde.Telefonummer = "29724034";
+
+            return View(kunde);
+        }
+
+    }
+}
---
+++ 2traceApp/Models/Kunde.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2traceApp.Models
+{
+    public class Kunde
+    {
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public string Telefonummer { get; set; }
+    }
+}
